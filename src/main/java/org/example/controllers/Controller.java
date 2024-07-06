5870f4a970045113e22636e64ad7270607352b62
Aleksandr Kuznetsov <kuznecovaleksandr923@gmail.com>
2024-07-06 23:24:12 +0300

    minor initial updates

--- src/main/java/org/example/controllers/Controller.java
+++ src/main/java/org/example/controllers/Controller.java
@@ -1,10 +1,12 @@
 package org.example.controllers;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.example.services.*;
 
+@org.springframework.stereotype.Controller
 @RequiredArgsConstructor
 public class Controller {
     private final Service service;
